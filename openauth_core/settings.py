1453a01947e0c6860fc7ab6a7403398152871e9d
amanuelcm27 <amanuelfirew27@gmail.com>
2025-12-23 13:46:43 +0300

    secret key hidden

--- openauth_core/settings.py
+++ openauth_core/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0lu2i_9u6%y+vc$5n4(z@)aycs&-iyw-1@ss7a8!b$9z@(l%xl'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
